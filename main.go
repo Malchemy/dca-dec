ba40d1cb0994381f1840589396e2cf5f3a46218d
Malchemy <cmcd799@gmail.com>
2019-06-15 00:46:48 -0400

    Update main.go

--- main.go
+++ main.go
@@ -38,8 +38,8 @@ func main() {
 		log.Println("usage:", os.Args[0], "[input]", "[output]")
 	}
 	
-	InFile := os.Args[1]
-	OutFile := os.Args[2]
+	//InFile := os.Args[1]
+	//OutFile := os.Args[2]
 	
 	fmt.Printf("%#v\n", flag.Args())
 	fmt.Println("Length of `x` is", len(flag.Args()))
