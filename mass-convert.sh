246d6bd868b0fefcfeb1412a08a6e20511fd79d0
Malchemy <cmcd799@gmail.com>
2019-06-25 00:55:22 -0400

    Update mass-convert.sh

--- mass-convert.sh
+++ mass-convert.sh
@@ -3,7 +3,15 @@
 # This will attempt to convert evert .dca in the current directory to a .wav
 # Does not work for files with spaces. Why do you have spaces in .dca's?
 
+# dca-dec must be in $PATH
 for FILE in *.dca
 {
-   ./dca-dec ${FILE} "$(basename "${FILE}" .dca).wav"
+   dca-dec ${FILE} "$(basename "${FILE}" .dca).wav"
 }
+
+# Comment out the top and use these lines if you'd rather have dca-dec
+# in the current folder instead of $PATH or just add ./ in front of dca-dec above..
+#for FILE in *.dca
+#{
+#   ./dca-dec ${FILE} "$(basename "${FILE}" .dca).wav"
+#}
